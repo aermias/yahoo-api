4f9a0e505d00f6a71fa5aa947d5d266b43e5623b
iVesperX <aermias@live.com>
2018-09-05 10:22:50 -0400

    initial commit

---
+++ src/main/java/aermias/yahoo_consume/YahooConsumeApplication.java
@@ -0,0 +1,12 @@
+package aermias.yahoo_consume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahooConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YahooConsumeApplication.class, args);
+	}
+}
---
+++ src/test/java/aermias/yahoo_consume/YahooConsumeApplicationTests.java
@@ -0,0 +1,16 @@
+package aermias.yahoo_consume;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YahooConsumeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
